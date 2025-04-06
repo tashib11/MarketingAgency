ff3a51314ea68cbbf168a246fe92ab08ab1801ec
tashib11 <islam2007011@stud.kuet.ac.bd>
2025-04-06 23:33:00 +0600

    axios added in layout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -63,6 +63,7 @@
  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
 </body>
 </html>
